6d3774a5e66b13478b01652a61d03cad3f498278
Reforgeted <1297738642@qq.com>
2024-03-20 23:50:01 +0800

    [Writer]Lux [Why]初始化项目，提交测试 [Thinking]别再空想，付出行动实践

---
+++ moving-now/src/main/java/com/lux/future/MovingUApplication.java
@@ -0,0 +1,12 @@
+package com.lux.future;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingUApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovingUApplication.class, args);
+    }
+}
